65e5e2b8dcdd133a3f29b6d29d11bdafbbbd8e8a
Yoni Calsin <yonicb20@gmail.com>
2020-06-15 10:09:07 -0500

    test: 💍 agregated more tessting sections

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -26,13 +26,26 @@ test('obtaining objects', () => {
    expect(env('DB')).toEqual({
       dialect: 'mysql',
       port: 336,
+      port_string: '336',
       localhost: 'localhost',
       user: 'root',
       password: 'cogenv_password',
       logging: true,
       database: 'cogenv_database',
+      URL: 'http://localhost:336',
    });
 
    // Database name
    expect(env('DB.database')).toEqual('cogenv_database');
+});
+
+// obtaining objects with typing
+test('obtaining objects with typing', () => {
+   expect(env('DB.port_string')).toEqual('336');
+   expect(env('DB.port')).toEqual(336);
+});
+
+// obtaining objects with interpolation
+test('obtaining objects with interpolation', () => {
+   expect(env('DB.URL')).toEqual('http://localhost:336');
 });
