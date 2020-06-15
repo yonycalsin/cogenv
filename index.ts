e68ca40053821cc05957596d9703ee7698f74901
Yoni Calsin <yonicb20@gmail.com>
2020-06-14 22:28:07 -0500

    feat: 🎸 Created default configuration for cogenv and its packag

---
+++ index.ts
@@ -0,0 +1,29 @@
+import { Config as Init, CogenvOptions, Use } from '@cogenv/core';
+import { CogenvType, CogenvTypeOptions } from '@cogenv/typed';
+import { CogenvObject } from '@cogenv/object';
+
+interface Options extends CogenvOptions {
+   typedOptions: CogenvTypeOptions;
+   [key: string]: any;
+}
+
+export const Config = (options: Options) => {
+   const { typedOptions, ...more } = options;
+
+   Init({
+      types: true,
+      objects: true,
+      logging: true,
+      ...more,
+   });
+
+   // initializing typing plugin
+   Use(CogenvType, typedOptions);
+
+   // Starting the object divider
+   Use(CogenvObject);
+
+   return cog.env || process.env;
+};
+
+export default Config;
